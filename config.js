2b874d932a8f15efa2d53ed4d1c430e7912cc0fd
Teerasak Mairoddee <teerasakmairoddee@Teerasaks-MacBook-Pro.local>
2021-11-12 01:19:01 +0000

    change to original code

---
+++ config.js
@@ -0,0 +1,9 @@
+var tokens = {
+    etherScanToken : "V52YIIN4MJQR7I9BS6S9YUDX861ZNHJ66Y",
+    polygonToken : "fVhcfgTm8mBGt9IcnNR5kb2d3kz5pxxh",
+    discordToken : "OTA0NDMxODA3NjUzNjEzNjQ4.YX7bwg.bCSt-WUxWL0TpVIMyrEJ6pUbUtU",
+    googleToken : "AIzaSyBh0G7D9Pk2bJ_eBiX-96lEkcJtT0fLZwQ"
+};
+
+module.exports = {tokens};
+
