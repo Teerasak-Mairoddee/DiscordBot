750bd2f9dbbc946c753285e1ddfb306af545b9ce
Teerasak Mairoddee <teerasakmairoddee@Teerasaks-MacBook-Pro.local>
2021-11-12 01:25:42 +0000

    change to private repo then redo key

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var tokens = {
     etherScanToken : "V52YIIN4MJQR7I9BS6S9YUDX861ZNHJ66Y",
     polygonToken : "fVhcfgTm8mBGt9IcnNR5kb2d3kz5pxxh",
-    discordToken : "OTA0NDMxODA3NjUzNjEzNjQ4.YX7bwg.bCSt-WUxWL0TpVIMyrEJ6pUbUtU",
+    discordToken : "OTA0NDMxODA3NjUzNjEzNjQ4.YX7bwg.spcFaL4qGaC11_1a86F2poc-yyE",
     googleToken : "AIzaSyBh0G7D9Pk2bJ_eBiX-96lEkcJtT0fLZwQ"
 };
 
